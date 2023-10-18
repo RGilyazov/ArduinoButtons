733c0d28f49f152b512302d85241b830823e7719
Ruslan Gliyazov <ruslan.g@yonalink.com>
2023-10-18 20:56:35 +0300

    wording

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include "classes/actions/PrintAction.h"
 #include "version.h"
 
-#define buttonPin 2        // analog input pin to use as a digital input
-#define ledPin 13          // digital output pin for the LED
+#define buttonPin 3       // analog input pin to use as a digital input
+#define ledPin 9          // digital output pin for the LED
 Button button;
 
 void setup() {
    button.onClick = new PrintAction("git push");
    button.onDoubleClick = new PrintAction("git pull");
-   button.onHold = new PrintAction("NICE :)))");
+   button.onHold = new PrintAction("For the World's Greatest Brother :)");
    button.onLongHold = new PrintAction(String("Version: ") 
     + VERSION
     + ". Source code can be found on https://github.com/RGilyazov/ArduinoButtons/tree/" 
